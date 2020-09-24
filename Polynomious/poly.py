6e44a8bf1de079edd69925f633c2905a916acde8
yamozha <yamozha@github.com>
2020-09-24 12:13:49 +0300

    tupak

--- Polynomious/poly.py
+++ Polynomious/poly.py
@@ -1,5 +1,23 @@
+# add threshold of division by the second polynomial
 import numpy as np
-x = np.array([1,6,11,6])
-y = np.array([1,0,-1])
+x = np.array([1,7,18,20,8])
+y = np.array([1,2,1])
 
-print(np.polydiv(x,y))
+print(f"Stepen na rezultata {len(x)-len(y)}")
+pedal, pedal2 = np.polydiv(x,y)
+pedalList = list(pedal)
+pedal2List = list(pedal2)
+
+a = list(map(int, range(len(pedalList))))
+a = a[::-1]
+
+print("Rezultat:")
+for i in range(len(pedalList)):
+    print(f"{int(pedalList[i])}x^{a[i]}")
+
+b = list(map(int, range(len(pedal2List)))) 
+b = b[::-1]
+
+print("Ostatuk")
+for i in range(len(pedal2List)):
+    print(f"{int(pedal2List[i])}x^{b[i]}")
