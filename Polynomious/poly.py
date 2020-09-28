3eff870bcec0ea486c640a7fd8238dda5a5898a6
yamozha <yamozha@github.com>
2020-09-28 12:07:06 +0300

    new shit

--- Polynomious/poly.py
+++ Polynomious/poly.py
@@ -1,23 +1,40 @@
-# add threshold of division by the second polynomial
+from flask import render_template, Flask, request
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 import numpy as np
-x = np.array([1,7,18,20,8])
-y = np.array([1,2,1])
+import requests
 
-print(f"Stepen na rezultata {len(x)-len(y)}")
-pedal, pedal2 = np.polydiv(x,y)
-pedalList = list(pedal)
-pedal2List = list(pedal2)
+app = Flask(__name__)
 
-a = list(map(int, range(len(pedalList))))
-a = a[::-1]
 
-print("Rezultat:")
-for i in range(len(pedalList)):
-    print(f"{int(pedalList[i])}x^{a[i]}")
+@app.route(/, methods=['post','get'])\
+def index():
 
-b = list(map(int, range(len(pedal2List)))) 
-b = b[::-1]
+    # add threshold of division by the second polynomial
+    x = np.array([1,-3,0,-4,0,1,-1])
+    y = np.array([1,1,1])
+    if request.method == "POST":
+        print(f"Stepen na rezultata {len(x)-len(y)}")
+        pedal, pedal2 = np.polydiv(x,y)
+        pedalList = list(pedal)
+        pedal2List = list(pedal2)
 
-print("Ostatuk")
-for i in range(len(pedal2List)):
-    print(f"{int(pedal2List[i])}x^{b[i]}")
+        a = list(map(int, range(len(pedalList))))
+        a = a[::-1]
+        publicListA = []
+        publicListB = []
+        
+        print("Rezultat:")
+        for i in range(len(pedalList)):
+            publicListA.append(int(pedalList[i])}x^{a[i])
+            print(f"{int(pedalList[i])}x^{a[i]}")
+            
+
+        b = list(map(int, range(len(pedal2List)))) 
+        b = b[::-1]
+
+        print("Ostatuk")
+        for i in range(len(pedal2List)):
+            publicListA.append(int(pedal2List[i])}x^{b[i])
+            print(f"{int(pedal2List[i])}x^{b[i]}")
---
+++ polyDiv/app.py
@@ -0,0 +1,50 @@
+from flask import render_template, Flask, request, flash, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+import numpy as np
+import requests
+
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+
+
+
+@app.route('/', methods=['post','get'])
+def index():
+    lastResult = str()
+    # add threshold of division by the second polynomial
+    if request.method == "POST":
+        publicListA = request.form.get('publicA')
+        publicListB = request.form.get('publicB')
+        x = np.array(list(map(int, publicListA.split(","))))
+        y = np.array(list(map(int, publicListB.split(","))))
+        print(x,y)
+        print(f"Stepen na rezultata {int(len(x)-len(y))}")
+        pedal, pedal2 = np.polydiv(x,y)
+        print(pedal, pedal2)
+        pedalList = list(pedal)
+        pedal2List = list(pedal2)
+        reformedPedal1 = []
+        reformedPedal2 = []
+        
+        a = list(map(int, range(len(pedalList))))
+        a = a[::-1]
+  
+        print("Rezultat:")
+        for i in range(len(pedalList)):
+            reformedPedal1.append(f"{int(pedalList[i])}x^{a[i]}")
+            print(f"{int(pedalList[i])}x^{a[i]}")
+            
+
+        b = list(map(int, range(len(pedal2List)))) 
+        b = b[::-1]
+
+        print("Ostatuk")
+        for i in range(len(pedal2List)):
+            reformedPedal2.append(f"{int(pedal2List[i])}x^{b[i]}")
+            print(f"{int(pedal2List[i])}x^{b[i]}")
+        flash(f"Rezultat = {reformedPedal1}, Ostatuk = {reformedPedal2}")
+    return render_template('index.html', lastResult=lastResult)
+
